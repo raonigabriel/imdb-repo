6060e88848b3f70fbfaa7692757105ac4023657c
Raoni Meira Gabriel <2515360+raonigabriel@users.noreply.github.com>
2020-06-25 10:28:07 -0300

    Create ArchitectureTest.java

---
+++ imdb-model/src/test/java/com/github/raonigabriel/imdb/ArchitectureTest.java
@@ -0,0 +1,59 @@
+package com.github.raonigabriel.imdb;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.GeneralCodingRules.ACCESS_STANDARD_STREAMS;
+import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
+import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JODATIME;
+
+import javax.persistence.Entity;
+
+import org.slf4j.Logger;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "com.github.raonigabriel.imdb")
+public class ArchitectureTest {
+
+	@ArchTest
+	private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+
+	@ArchTest
+	private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+
+	@ArchTest
+	private final ArchRule no_jodatime = NO_CLASSES_SHOULD_USE_JODATIME;
+
+	@ArchTest
+	private final ArchRule no_field_injection = NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
+	
+	@ArchTest
+	private final ArchRule no_access_to_standard_streams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+	
+	@ArchTest
+	private final ArchRule entities_on_same_package = classes().that()
+		.areAnnotatedWith(Entity.class)
+		.should().resideInAPackage("com.github.raonigabriel.imdb.model")
+		.because("All entities should be in the same package");
+
+	@ArchTest
+	private void no_access_to_standard_streams_as_method(JavaClasses classes) {
+		noClasses().should(ACCESS_STANDARD_STREAMS).check(classes);
+	}
+
+	@ArchTest
+	private final ArchRule loggers_should_be_private_static_final = fields().that()
+		.haveRawType(Logger.class)
+		.should().bePrivate()
+		.andShould().beStatic()
+		.andShould().beFinal()
+		.because("we agreed on this convention");
+
+}
